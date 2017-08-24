18e67fa3203d60cca7870cfbabc0ceb9fe3ef4d1
werk <mfwerk@gmail.com>
2017-08-24 22:17:09 +0200

    correct rotation

--- src/main/scala/dk/mzw/scalasprites/gl/QuadGl.scala
+++ src/main/scala/dk/mzw/scalasprites/gl/QuadGl.scala
@@ -9,6 +9,7 @@ import scala.scalajs.js.typedarray.Float32Array
 class QuadGl(canvas : HTMLCanvasElement) {
 
     var coordinatesAttributeLocation : Int = _
+    var extraAttributeLocation : Int = _
     var samplerUniformLocation : WebGLUniformLocation = _
     var scaleUniformLocation : WebGLUniformLocation = _
     var offsetUniformLocation : WebGLUniformLocation = _
@@ -18,9 +19,9 @@ class QuadGl(canvas : HTMLCanvasElement) {
     def initSpriteProgram() : Unit = {
         val vertexCode = """
             attribute vec4 a_coordinates;
+            attribute vec2 a_extra;
             uniform vec2 u_scale;
             uniform vec2 u_offset;
-            //attribute vec2 a_textureCoordinates;
 
             varying vec2 v_textureCoordinates;
 
@@ -32,7 +33,7 @@ class QuadGl(canvas : HTMLCanvasElement) {
                 float s = sin(angle);
                 float c = cos(angle);
                 mat2 rotate = mat2(c, s, -s, c);
-                vec2 p = rotate * a_coordinates.xy;
+                vec2 p = rotate * (a_coordinates.xy - a_extra) + a_extra;
                 gl_Position = vec4(p * u_scale + u_offset, 1.0, 1.0);
                 v_textureCoordinates = vec2(tx, ty);
             }
@@ -53,6 +54,7 @@ class QuadGl(canvas : HTMLCanvasElement) {
         gl.useProgram(program)
 
         coordinatesAttributeLocation = gl.getAttribLocation(program, "a_coordinates")
+        extraAttributeLocation = gl.getAttribLocation(program, "a_extra")
         samplerUniformLocation = gl.getUniformLocation(program, "u_sampler")
         scaleUniformLocation = gl.getUniformLocation(program, "u_scale")
         offsetUniformLocation = gl.getUniformLocation(program, "u_offset")
@@ -87,29 +89,36 @@ class QuadGl(canvas : HTMLCanvasElement) {
             )
         } : _*)
 
-        // Create an empty buffer object to store the vertex buffer
+        val verticesExtra = js.Array(points.flatMap{case (cx, cy, _, _, _) =>
+            Array(
+                cx, cy,
+                cx, cy,
+                cx, cy,
+                cx, cy,
+                cx, cy,
+                cx, cy
+            )
+        } : _*)
+
         val vertexBuffer = gl.createBuffer()
-
-        //Bind appropriate array buffer to it
         gl.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer)
-
-        // Pass the vertex data to the buffer
         gl.bufferData(GL.ARRAY_BUFFER, new Float32Array(vertices), GL.STREAM_DRAW)
+        gl.bindBuffer(GL.ARRAY_BUFFER, null)
 
-        // Unbind the buffer
+        val vertexExtraBuffer = gl.createBuffer()
+        gl.bindBuffer(GL.ARRAY_BUFFER, vertexExtraBuffer)
+        gl.bufferData(GL.ARRAY_BUFFER, new Float32Array(verticesExtra), GL.STREAM_DRAW)
         gl.bindBuffer(GL.ARRAY_BUFFER, null)
 
         /*======== Associating shaders to buffer objects ========*/
 
-        // Bind vertex buffer object
         gl.bindBuffer(GL.ARRAY_BUFFER, vertexBuffer)
-
-
-        // Point an attribute to the currently bound VBO
         gl.vertexAttribPointer(coordinatesAttributeLocation, 4, GL.FLOAT, normalized = false, 0, 0)
-
-        // Enable the attribute
         gl.enableVertexAttribArray(coordinatesAttributeLocation)
+
+        gl.bindBuffer(GL.ARRAY_BUFFER, vertexExtraBuffer)
+        gl.vertexAttribPointer(extraAttributeLocation, 2, GL.FLOAT, normalized = false, 0, 0)
+        gl.enableVertexAttribArray(extraAttributeLocation)
 
         /*============= Drawing the primitive ===============*/
 
