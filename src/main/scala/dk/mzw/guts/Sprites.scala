a091ff2114692d201428cd26b7fd1c13b9ec4d91
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2017-09-27 18:25:30 +0200

    More ground textures.

--- src/main/scala/dk/mzw/guts/Sprites.scala
+++ src/main/scala/dk/mzw/guts/Sprites.scala
@@ -7,7 +7,7 @@ class Sprites(loader : Loader) {
     val bunny = loader("bunnymark/rabbitv3_batman.png")
     val wall = loader("assets/wall2.png", repeat = true)
     val floor = loader("assets/floor.png")
-    val ground = loader("assets/ground.png", repeat = true)
+    val ground = loader("assets/ground2.png", repeat = true)
     val barrel = loader("assets/barrelGreen_up.png")
 
     val topManAnimation = loader("assets/topman.png").split(24, 4)
