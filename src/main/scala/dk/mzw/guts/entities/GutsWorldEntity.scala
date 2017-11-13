6398b3145f9ebdcad59ce88a001691b5756b9a1a
Ravnsmed <mfwerk@gmail.com>
2017-11-13 21:38:25 +0100

    100 monsters from the beginning

--- src/main/scala/dk/mzw/guts/entities/GutsWorldEntity.scala
+++ src/main/scala/dk/mzw/guts/entities/GutsWorldEntity.scala
@@ -13,21 +13,6 @@ class GutsWorldEntity(self : Self, sprites : Sprites) extends WorldEntity(self, 
     val world = this
 
     override def internalUpdate(boundingBox : SpriteCanvas.BoundingBox, mouse : Mouse, delta : Double) : Unit = {
-        if(Math.random() < 0.3 * delta) {
-            var r = Math.random()
-            val v =
-                if(r < 0.25) Vector2d(boundingBox.x1 - 5, Math.random() * boundingBox.height)
-                else if(r < 0.50) Vector2d(boundingBox.x2 + 5, Math.random() * boundingBox.height)
-                else if(r < 0.75) Vector2d(Math.random() * boundingBox.width, boundingBox.y1 - 5)
-                else Vector2d(Math.random() * boundingBox.width, boundingBox.y2 + 5)
-            r = Math.random()
-            val m =
-                if(r < 0.25) SpawnSkeleton(Self(), v)
-                else if(r < 0.50) SpawnZombie(Self(), v)
-                else if(r < 0.75) SpawnScorpion(Self(), v)
-                else SpawnWolf(Self(), v)
-            sendMessageTo(this, m)
-        }
         super.internalUpdate(boundingBox, mouse, delta)
     }
 
@@ -55,7 +40,7 @@ class GutsWorldEntity(self : Self, sprites : Sprites) extends WorldEntity(self, 
                 }
                 i += 1
             }
-        case SpawnBunny(thatSelf, position) =>
+        case SpawnPlayer(thatSelf, position) =>
             entities.push(new PlayerEntity(this, thatSelf, position, sprites.topManAnimation, sprites.topManShootingAnimation))
         case SpawnSkeleton(thatSelf, position) =>
             entities.push(new SkeletonEntity(this, thatSelf, position, 4, 100, sprites.skeleton))
@@ -90,7 +75,7 @@ class GutsWorldEntity(self : Self, sprites : Sprites) extends WorldEntity(self, 
 
 object GutsWorldEntity {
     case class Unspawn(self : Self) extends Message
-    case class SpawnBunny(self : Self, position : Vector2d) extends Message
+    case class SpawnPlayer(self : Self, position : Vector2d) extends Message
     case class SpawnSkeleton(self : Self, position : Vector2d) extends Message
     case class SpawnZombie(self : Self, position : Vector2d) extends Message
     case class SpawnScorpion(self : Self, position : Vector2d) extends Message
