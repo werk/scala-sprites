202b4e4a8f7948e640e250f57867179f7093bf4f
werk <mfwerk@gmail.com>
2017-12-17 22:56:29 +0100

    Puzzle: Better but still wrong grouping

--- src/main/scala/dk/mzw/puzzle/Puzzle.scala
+++ src/main/scala/dk/mzw/puzzle/Puzzle.scala
@@ -38,7 +38,10 @@ object Puzzle extends JSApp {
         def dragEnd(p : Piece, x : Double, y : Double): Unit = {
             if(x < borderMin || borderMax < x || y < borderMin || borderMax < y) {
             } else {
-                p.group.pieces.foreach{moved =>
+                val directionX = Math.signum(p.group.offsetX)
+                val directionY = Math.signum(p.group.offsetY)
+                val sorted = p.group.members.sortBy(p => (-directionX * p.current._1, -directionY * p.current._2))
+                sorted.foreach{moved =>
                     val offset = board.center(moved.current._1 + p.group.offsetX, moved.current._2 + p.group.offsetY)
                     board.pieces.get(offset).foreach{blocking =>
                         board.swap(blocking, moved)
@@ -97,7 +100,7 @@ class Board(size : Int) {
         } yield (x, y)
 
         positions.zip(scala.util.Random.shuffle(positions)).map{case (home, current) =>
-            current -> Piece(home, current, Group(Set(), 0, 0))
+            current -> Piece(home, current, Group(List(), 0, 0))
         }.toMap
     }
     reGroup()
@@ -119,7 +122,7 @@ class Board(size : Int) {
 
     def reGroup() = {
         pieces = pieces.map{case (_, p) =>
-            p.group.pieces = Set(p)
+            p.group = Group(List(p), 0, 0)
             p.current -> p
         }
 
@@ -141,9 +144,14 @@ class Board(size : Int) {
             val piece1 = pieces(p1)
             val piece2 = pieces(p2)
             if(piece1.delta == piece2.delta) {
-                piece1.group.pieces += piece2
-                piece2.group.pieces += piece1
+                val members = piece1.group.members ++ piece2.group.members
+                val group = piece1.group.copy(members = members)
+                members.foreach(_.group = group)
             }
+        }
+        println("Re-grouped:")
+        pieces.foreach { case (_, p) =>
+            println(s"  ${p.home} -> ${p.current}: GROUP-${p.group.id} ${p.group.members.map(_.home).mkString(", ")}")
         }
     }
 }
@@ -157,137 +165,11 @@ object Board {
         def delta : (Int, Int) = (current._1 - home._1, current._2 - home._2)
     }
 
+    var groupId = 0
     case class Group(
-        var pieces : Set[Piece],
+        var members : List[Piece],
         var offsetX : Double,
-        var offsetY : Double
+        var offsetY : Double,
+        id : Double = {groupId += 1; groupId}
     )
 }
-
-
-/*
-
-object Puzzle extends JSApp {
-
-    case class Piece(
-        var x : Double,
-        var y : Double,
-        imageX : Double,
-        imageY : Double,
-        var group : List[Piece]
-    )
-
-    case class GrabbedPiece(
-        piece : Piece,
-        grabX : Double,
-        grabY : Double,
-        initialX : Double,
-        initialY : Double
-    )
-
-    def main(): Unit = {
-        val canvas = dom.document.getElementById("spriteCanvas").asInstanceOf[HTMLCanvasElement]
-        val loader = new Loader(canvas)
-        val animation = loader(Animations.ballz)
-        val floor = loader("assets/floor.png")
-
-
-        val size = 4
-        val edge = 2.0 / size
-        val halfEdge = edge / 2.0
-        val halfSize = size / 2.0
-
-        def center(x : Double) = (Math.round(x * halfSize + 0.5) - 0.5) / halfSize
-
-        val positions = (for{
-            ix <- 0 until size
-            x = center(ix.toDouble / size * 2 - 1 + halfEdge)
-            iy <- 0 until size
-            y = center(iy.toDouble / size * 2 - 1 + halfEdge)
-        } yield (x, y)).toList
-
-        val pieces = positions.zip(/*scala.util.Random.shuffle*/(positions)).map{case ((x, y), (x2, y2)) =>
-            val piece = Piece(
-                x = x2,
-                y = y2,
-                imageX = x - halfEdge,
-                imageY = y - halfEdge,
-                group = List()
-            )
-            piece.group = List(piece)
-            piece
-        }
-
-        def findPiece(x : Double, y : Double, notThis : Option[Piece] = None) : Option[Piece] = {
-            pieces.find{p =>
-                p.x - halfEdge < x && x < p.x + halfEdge &&
-                    p.y - halfEdge < y && y < p.y + halfEdge && !notThis.contains(p)
-            }
-        }
-
-        def dragStart(x : Double, y : Double) : Option[GrabbedPiece] = {
-            findPiece(x, y).map{p =>
-                GrabbedPiece(
-                    p,
-                    x - p.x,
-                    y - p.y,
-                    initialX = p.x,
-                    initialY = p.y
-                )
-            }
-        }
-
-        def dragContinue(p : GrabbedPiece, x : Double, y : Double) {
-            p.piece.x = x - p.grabX
-            p.piece.y = y - p.grabY
-        }
-
-        def dragEnd(g : GrabbedPiece, x : Double, y : Double): Unit = {
-            if(x < -1 || 1 < x || y < -1 || 1 < y) {
-                g.piece.x = g.initialX
-                g.piece.y = g.initialY
-            } else {
-                findPiece(x, y, Some(g.piece)).foreach { other =>
-                    other.x = g.initialX
-                    other.y = g.initialY
-                }
-                g.piece.x = center(x)
-                g.piece.y = center(y)
-            }
-        }
-
-        loader.complete.foreach { display =>
-            val mouse = new MouseDrag[GrabbedPiece](canvas, display.gameCoordinatesX, display.gameCoordinatesY, dragStart, dragContinue, dragEnd)
-
-            // This crazy stuff is done to avoid creating and allocating a new anonymous function for each call to requestAnimationFrame
-            var loopF : Double => Unit = null
-
-            val start : Double = secondsElapsed() - 0.01
-            def loop(_t : Double) : Unit = {
-                val t = start - secondsElapsed()
-                display.add(floor, -10, -10, 1, 0) // TODO remove
-                val image = animation(t)
-                pieces.foreach{piece =>
-                    display.add(
-                        image = image,
-                        imageX = piece.imageX,
-                        imageY = piece.imageY,
-                        imageWidth = edge,
-                        imageHeight = edge,
-                        x = piece.x,
-                        y = piece.y,
-                        width = edge,
-                        height = edge,
-                        angle = 0,
-                        blending = Blending.additive
-                    )
-                }
-                display.draw((0,0,0,1), 2)
-                dom.window.requestAnimationFrame(loopF)
-            }
-            loopF = loop
-            loop(0)
-        }
-    }
-}
- */
